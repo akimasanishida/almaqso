3ac145d1787e9d78564b343f96091cbb71691271
Akimasa Nishida <akimasa.nishida@grad.nao.ac.jp>
2025-01-22 15:32:54 +0900

    Unverified

--- almaqso/_qsoanalysis.py
+++ almaqso/_qsoanalysis.py
@@ -1,15 +1,16 @@
 import os
-import sys
+import shutil
 import numpy as np
 import glob
+from itertools import chain
 from casatasks import importasdm, mstransform
 import analysisUtils as aU
 import almaqa2csg as csg
 
 
-def _writelog(content: str):
-    os.system('touch almaqso_analysis.log')
-    os.system(f'echo "{content}" >> almaqso_analysis.log')
+# def _writelog(content: str):
+#     os.system('touch almaqso_analysis.log')
+#     os.system(f'echo "{content}" >> almaqso_analysis.log')
 
 
 def _qsoanalysis(tarfilename, casacmd):
@@ -36,7 +37,7 @@ def _qsoanalysis(tarfilename, casacmd):
     os.system(f'rm -rf {kw_importasdm["vis"]}')
     importasdm(**kw_importasdm)
 
-    _writelog('Step 1. Import the ASDM file is done.')
+    casalog.post('Step 1. Import the ASDM file is done.')
 
     # Step 2. Generate a calibration script
     try:
@@ -49,10 +50,10 @@ def _qsoanalysis(tarfilename, casacmd):
     if os.path.exists(
             f'./log/{visname}.scriptForCalibration.py'
         ):
-        print('Calibration script already exists.')
+        # print('Calibration script already exists.')
         os.system(f'cp ./log/{visname}.scriptForCalibration.py ./')
     else:
-        print('Calibration script will be generated.')
+        # print('Calibration script will be generated.')
         csg.generateReducScript(
                 msNames = vis,
                 refant = refant[0],
@@ -65,78 +66,61 @@ def _qsoanalysis(tarfilename, casacmd):
 
     dish_diameter = aU.almaAntennaDiameter(refant[0])
 
-    _writelog('Step 2. Generate a calibration script is done.')
+    casalog.post('Step 2. Generate a calibration script is done.')
 
     # Step 3. Remove the target source
-    IntentListASDM = aU.getIntentsFromASDM(asdmfile)
+    intentListASDM = aU.getIntentsFromASDM(asdmfile)
+    intentList = list(chain.from_iterable(intentListASDM.values()))
+    intentList_unique = np.unique(intentList)
+    intentList_non_target = intentList_unique[intentList_unique != 'OBSERVE_TARGET']
 
-    IntentList = []
-    for intfield in list(IntentListASDM):
-        IntentList = IntentList + IntentListASDM[intfield]
+    shutil.rmtree(visname + '.org', ignore_errors=True)
+    shutil.move(visname, visname + '.org')
 
-    listOfIntents_init = (np.unique(IntentList)[
-                          np.unique(IntentList) != 'OBSERVE_TARGET'])
-
-    os.system('rm -rf '+visname+'.org')
-    os.system('mv '+visname+' '+visname+'.org')
     kw_mstransform = {
-        'vis': visname+'.org',
+        'vis': visname + '.org',
         'outputvis': visname,
         'datacolumn': 'all',
-        'intent': '*,'.join(listOfIntents_init)+'*',
+        'intent': '*,'.join(intentList_non_target) + '*',
         'keepflags': True
     }
 
-    os.system('rm -rf '+kw_mstransform['outputvis'])
-    os.system('rm -rf '+kw_mstransform['outputvis']+'.flagversions')
+    shutil.rmtree(kw_mstransform['outputvis'], ignore_errors=True)
+    shutil.rmtree(kw_mstransform['outputvis'] + '.flagversions', ignore_errors=True)
+
     mstransform(**kw_mstransform)
 
-    _writelog('Step 3. Remove the target source is done.')
+    casalog.post('Step 3. Remove the target source is done.')
 
     # Step 4. Calibration
     cmdfile = visname + '.scriptForCalibration.py'
 
-    checksteps = open(cmdfile, 'r')
-    syscalcheck = checksteps.readlines().copy()[21]
-    checksteps.close()
+    with open(cmdfile, 'r') as f:
+        syscalcheck = f.readlines().copy()[21]
 
-    f = open(cmdfile.replace('.py', '.part.py'), 'w')
+    # checksteps = open(cmdfile, 'r')
+    # syscalcheck = checksteps.readlines().copy()[21]
+    # checksteps.close()
+
+    # f = open(cmdfile.replace('.py', '.part.py'), 'w')
     if syscalcheck.split(':')[1].split("'")[1] == \
             'Application of the bandpass and gain cal tables':
-        f.write(
-            'mysteps = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]'+'\n')
+        # f.write(
+        #     'mysteps = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]'+'\n')
+        mysteps = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     else:
-        f.write(
-            'mysteps = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]'+'\n')
-    f.write('applyonly = True'+'\n')
-    f.write('execfile('+'"'+cmdfile+'"'+',globals())'+'\n')
-    f.close()
+        # f.write(
+        #     'mysteps = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]'+'\n')
+        mysteps = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    # f.write('applyonly = True'+'\n')
+    # f.write('execfile('+'"'+cmdfile+'"'+',globals())'+'\n')
+    # f.close()
+    applyonly = True
+    execfile(cmdfile, globals())
 
-    cmd = '"' + \
-        'execfile('+"'"+cmdfile.replace('.py',
-                                        '.part.py')+"'"+')' + '"'
-    os.system(casacmd+' --nologger --nogui -c '+cmd)
+    # cmd = '"' + \
+    #     'execfile('+"'"+cmdfile.replace('.py',
+    #                                     '.part.py')+"'"+')' + '"'
+    # os.system(casacmd+' --nologger --nogui -c '+cmd)
 
-    vis = visname + '.split'
-
-    fields = np.unique(aU.getCalibrators(vis=vis))
-    fields = []
-
-    for field in fields:
-        if field[0] == 'J':
-            fields.append(field)
-
-    beamsize = aU.estimateSynthesizedBeam(vis)
-    from casatools import synthesisutils
-    su = synthesisutils()
-
-    if dish_diameter > 10.:
-        # 12m
-        imsize = su.getOptimumSize(int(120./beamsize*5))
-    else:
-        # 7m
-        imsize = su.getOptimumSize(int(180./beamsize*5))
-
-    cell = '{:.3f}'.format(beamsize/5) + 'arcsec'
-
-    _writelog('Step 4. Calibration is done.')
+    casalog.post('Step 4. Calibration is done.')
--- almaqso/analysis.py
+++ almaqso/analysis.py
@@ -2,16 +2,17 @@ import os
 import subprocess
 
 
-def _run_casa_cmd(casa, cmd):
+def _run_casa_cmd(casa, mpicasa, n_core, cmd, verbose):
     try:
         result = subprocess.run(
-            [casa, '--nologger', '--nogui', '-c', cmd],
+            [mpicasa, '-n', n_core, casa, '--nologger', '--nogui', '-c', cmd],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
-        print(f"STDOUT for {cmd}:", result.stdout)
+        if verbose:
+            print(f"STDOUT for {cmd}:", result.stdout)
         print(f"STDERR for {cmd}:", result.stderr)
     except subprocess.CalledProcessError as e:
         print(f"Error while executing {cmd}:")
@@ -21,14 +22,20 @@ def _run_casa_cmd(casa, cmd):
         raise
 
 
-def analysis(tardir: str, casacmd='casa', skip=True):
+def analysis(tardir: str, casapath, mpicasa='mpicasa', n_core=8, skip=True, verbose=False) -> None:
     """
     Run the analysis of the QSO data.
 
     Args:
         tardir (str): Directory containing the `*.asdm.sdm.tar` files.
-        casacmd (str): CASA command. Default is 'casa'.
+        casapath (str): Path to the CASA executable.
+        mpicasa (str): Path to the MPI CASA executable. Default is 'mpicasa'.
+        n_core (int): Number of cores to use for the analysis. Default is 8.
         skip (bool): Skip the analysis if the output directory exists. Default is True.
+        verbose (bool): Print the STDOUT of the CASA commands. Default is False.
+
+    Returns:
+        None
     """
     asdm_files = [file for file in os.listdir(f'{tardir}') if file.endswith('.asdm.sdm.tar')]
     almaqso_dir = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
@@ -49,6 +56,6 @@ def analysis(tardir: str, casacmd='casa', skip=True):
             cmd = f"sys.path.append('{almaqso_dir}');" + \
                 "from almaqso._qsoanalysis import _qsoanalysis;" + \
                 f"_qsoanalysis('{asdm_file}', '{casacmd}')"
-            _run_casa_cmd(casacmd, cmd)
+            _run_casa_cmd(casacmd, cmd, verbose)
 
             os.chdir('..')
