c9a930c15115559fe54590254e2b7e6dcae33115
Akimasa Nishida <akimasa.nishida@grad.nao.ac.jp>
2025-01-24 15:30:51 +0900

    minor update of test and sample

--- sample/sample.py
+++ sample/sample.py
@@ -3,4 +3,4 @@ from almaqso import download_archive, analysis
 
 if __name__ == '__main__':
     download_archive(4, 'catalog/test_2.json')
-    analysis('.', casacmd='/usr/local/casa/casa-6.6.1-17-pipeline-2024.1.0.8/bin/casa')  # Specify the path to the CASA binary
+    analysis('.', casapath='/usr/local/casa/casa-6.6.1-17-pipeline-2024.1.0.8/bin/casa')  # Specify the path to the CASA binary
