9d5054aec0053a4af153311659a111a1a9549fb2
Akimasa Nishida <akimasa.nishida@grad.nao.ac.jp>
2025-03-27 11:46:08 +0900

    temporary

--- test/test_download_analysis.py
+++ test/test_download_analysis.py
@@ -3,13 +3,14 @@ import sys
 import shutil
 from pathlib import Path
 sys.path.append('../almaqso')  # isort:skip
-from almaqso import download_archive, analysis
+from almaqso.download_archive import download_archive
+from almaqso.analysis import analysis
 
 
 CURRENT_DIR = Path(__file__).parent
 CASA_PATH = '/usr/local/casa/casa-6.6.1-17-pipeline-2024.1.0.8/bin/casa'  # Path to CASA. Please modify this if necessary.
 DATA_PATH = './uid___A002_Xd68367_X9885'
-TARFILE = '../2018.1.01575.S_uid___A002_Xd68367_X9885.asdm.sdm.tar'
+TARFILE = './2018.1.01575.S_uid___A002_Xd68367_X9885.asdm.sdm.tar'
 
 # Edit the following constants.
 DOWNLOAD = False  # True: Download the tar file, False: Use the existing tar file
