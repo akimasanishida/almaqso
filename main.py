12ef2f6cc4da4a332774c02ff797d1ea02b6a003
Akimasa Nishida <akimasa.nishida@grad.nao.ac.jp>
2025-12-27 16:24:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,28 @@ from almaqso import Almaqso
 
 if __name__ == "__main__":
     almaqso = Almaqso(
-        # target=["J0211+1051"],
-        # band=3,
-        target=["J2000-1748"],
-        band=4,
+        target=["J0211+1051"],
+        band=3,
         cycle="",
-        work_dir="./test_dir",
+        work_dir="./test_dir_J0211+1051_band3/",
+        # target=["J2000-1748"],
+        # band=4,
+        # cycle="",
+        # target=["J1832-1035"],
+        # band=6,
+        # cycle="8",
+        # work_dir="./test_dir_J1832-1035_band6_cycle8/",
         casapath="/usr/local/casa/casa-6.6.6-17-pipeline-2025.1.0.35-py3.10.el8/bin/casa"
     )
     almaqso.process(
         n_parallel=2,
+        skip_previous_successful=True,
         do_tclean=True,
         tclean_mode=["mfs", "mfs_spw", "cube"],
         # do_selfcal=True,
         # kw_selfcal={},
         do_export_fits=True,
+        remove_casa_images=True,
         remove_asdm=True,
         remove_intermediate=True,
     )
