b916fcdeca3f0179c96d988cc4bc8efb6ba9b372
Akimasa Nishida <90087433+akimasanishida@users.noreply.github.com>
2026-01-16 16:07:37 +0900

    Only retain user's target

--- almaqso/_templates/_remove_target.py
+++ almaqso/_templates/_remove_target.py
@@ -1,15 +1,12 @@
 import analysisUtils as aU
 
 
-fields = aU.getFields("{vis}")
-fields_target = aU.getTargetsForIntent("{vis}")
-fields_cal = list(set(fields) - set(fields_target))
-print(fields_cal)
+fields = {retain_fields}
 
 kw_split = {{
     "vis": "{vis}",
     "outputvis": "{vis}.split",
-    "field": ", ".join(fields_cal),
+    "field": ", ".join(fields),
     "datacolumn": "all",
 }}
 
