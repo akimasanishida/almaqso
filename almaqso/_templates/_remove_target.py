f1d246dc94d4b70df81db2fbc2dc5792651b31e2
Akimasa Nishida <90087433+akimasanishida@users.noreply.github.com>
2026-01-17 16:38:39 +0900

    Restore process to remove science target from _remove_target.py

--- almaqso/_templates/_remove_target.py
+++ almaqso/_templates/_remove_target.py
@@ -1,12 +1,14 @@
 import analysisUtils as aU
 
 
-fields = {retain_fields}
+fields = {retain_fields}  # Target fields
+fields_science = aU.getTargetsForIntent("{vis}")  # Science target fields
+fields_selected = [f for f in fields if f not in fields_science]  # Remove science targets
 
 kw_split = {{
     "vis": "{vis}",
     "outputvis": "{vis}.split",
-    "field": ", ".join(fields),
+    "field": ", ".join(fields_selected),
     "datacolumn": "all",
 }}
 
